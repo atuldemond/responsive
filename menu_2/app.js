02616fdc258de2b80530c00348dd6f1788d639e2
atuldemond <atuldemond@gmail.com>
2024-07-13 11:27:30 +0530

    proper responsive menu is completed today

---
+++ menu_2/app.js
@@ -0,0 +1,17 @@
+let logo1 = document.querySelector("#logo1");
+let logo2 = document.querySelector("#logo2");
+let menu = document.querySelector(".menu");
+
+logo1.addEventListener("click", function () {
+  menu.classList.toggle("openmenu");
+  menu.style.left = "0px";
+  logo2.style.display = "block";
+  logo1.style.display = "none";
+});
+
+logo2.addEventListener("click", function () {
+  menu.classList.toggle("openmenu");
+  menu.style.left = "0px";
+  logo2.style.display = "none";
+  logo1.style.display = "block";
+});
